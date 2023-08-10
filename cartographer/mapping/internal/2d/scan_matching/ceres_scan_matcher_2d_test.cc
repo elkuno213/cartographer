92072e2a6ea9670e3fa6940e3ae647c7dd8b6ca2
Vu Hoi HUYNH <>
2023-08-10 10:51:37 +0200

    format: ceres scan matcher 2d

--- cartographer/mapping/internal/2d/scan_matching/ceres_scan_matcher_2d_test.cc
+++ cartographer/mapping/internal/2d/scan_matching/ceres_scan_matcher_2d_test.cc
@@ -14,18 +14,18 @@
  * limitations under the License.
  */
 
-#include "cartographer/mapping/internal/2d/scan_matching/ceres_scan_matcher_2d.h"
-
 #include <memory>
 
 #include "absl/memory/memory.h"
+#include "gtest/gtest.h"
+
 #include "cartographer/common/internal/testing/lua_parameter_dictionary_test_helpers.h"
 #include "cartographer/common/lua_parameter_dictionary.h"
 #include "cartographer/mapping/2d/probability_grid.h"
+#include "cartographer/mapping/internal/2d/scan_matching/ceres_scan_matcher_2d.h"
 #include "cartographer/mapping/probability_values.h"
 #include "cartographer/sensor/point_cloud.h"
 #include "cartographer/transform/rigid_transform_test_helpers.h"
-#include "gtest/gtest.h"
 
 namespace cartographer {
 namespace mapping {
@@ -33,14 +33,16 @@ namespace scan_matching {
 namespace {
 
 class CeresScanMatcherTest : public ::testing::Test {
- protected:
+protected:
   CeresScanMatcherTest()
-      : probability_grid_(
-            MapLimits(1., Eigen::Vector2d(10., 10.), CellLimits(20, 20)),
-            &conversion_tables_) {
+    : probability_grid_(
+      MapLimits(1., Eigen::Vector2d(10., 10.), CellLimits(20, 20)),
+      &conversion_tables_
+    ) {
     probability_grid_.SetProbability(
-        probability_grid_.limits().GetCellIndex(Eigen::Vector2f(-3.5f, 2.5f)),
-        kMaxProbability);
+      probability_grid_.limits().GetCellIndex(Eigen::Vector2f(-3.5f, 2.5f)),
+      kMaxProbability
+    );
 
     point_cloud_.push_back({Eigen::Vector3f{-3.f, 2.f, 0.f}});
 
@@ -55,23 +57,28 @@ class CeresScanMatcherTest : public ::testing::Test {
             num_threads = 1,
           },
         })text");
-    const proto::CeresScanMatcherOptions2D options =
-        CreateCeresScanMatcherOptions2D(parameter_dictionary.get());
+    const proto::CeresScanMatcherOptions2D options
+      = CreateCeresScanMatcherOptions2D(parameter_dictionary.get());
     ceres_scan_matcher_ = absl::make_unique<CeresScanMatcher2D>(options);
   }
 
   void TestFromInitialPose(const transform::Rigid2d& initial_pose) {
     transform::Rigid2d pose;
-    const transform::Rigid2d expected_pose =
-        transform::Rigid2d::Translation({-0.5, 0.5});
+    const transform::Rigid2d expected_pose
+      = transform::Rigid2d::Translation({-0.5, 0.5});
     ceres::Solver::Summary summary;
-    ceres_scan_matcher_->Match(initial_pose.translation(), initial_pose,
-                               point_cloud_, probability_grid_, &pose,
-                               &summary);
+    ceres_scan_matcher_->Match(
+      initial_pose.translation(),
+      initial_pose,
+      point_cloud_,
+      probability_grid_,
+      &pose,
+      &summary
+    );
     EXPECT_NEAR(0., summary.final_cost, 1e-2) << summary.FullReport();
     EXPECT_THAT(pose, transform::IsNearly(expected_pose, 1e-2))
-        << "Actual: " << transform::ToProto(pose).DebugString()
-        << "\nExpected: " << transform::ToProto(expected_pose).DebugString();
+      << "Actual: " << transform::ToProto(pose).DebugString()
+      << "\nExpected: " << transform::ToProto(expected_pose).DebugString();
   }
 
   ValueConversionTables conversion_tables_;
@@ -96,7 +103,7 @@ TEST_F(CeresScanMatcherTest, testOptimizeAlongXY) {
   TestFromInitialPose(transform::Rigid2d::Translation({-0.3, 0.3}));
 }
 
-}  // namespace
-}  // namespace scan_matching
-}  // namespace mapping
-}  // namespace cartographer
+} // namespace
+} // namespace scan_matching
+} // namespace mapping
+} // namespace cartographer
