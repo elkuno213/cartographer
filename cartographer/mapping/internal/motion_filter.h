3f60c797d926d4584e81761948430a4a4bab9795
Vu Hoi HUYNH <>
2023-08-17 17:05:11 +0000

    format: motion filter

--- cartographer/mapping/internal/motion_filter.h
+++ cartographer/mapping/internal/motion_filter.h
@@ -28,11 +28,12 @@ namespace cartographer {
 namespace mapping {
 
 proto::MotionFilterOptions CreateMotionFilterOptions(
-    common::LuaParameterDictionary* parameter_dictionary);
+  common::LuaParameterDictionary* parameter_dictionary
+);
 
 // Takes poses as input and filters them to get fewer poses.
 class MotionFilter {
- public:
+public:
   explicit MotionFilter(const proto::MotionFilterOptions& options);
 
   // If the accumulated motion (linear, rotational, or time) is above the
@@ -40,15 +41,15 @@ class MotionFilter {
   // true is returned.
   bool IsSimilar(common::Time time, const transform::Rigid3d& pose);
 
- private:
+private:
   const proto::MotionFilterOptions options_;
-  int num_total_ = 0;
+  int num_total_     = 0;
   int num_different_ = 0;
   common::Time last_time_;
   transform::Rigid3d last_pose_;
 };
 
-}  // namespace mapping
-}  // namespace cartographer
+} // namespace mapping
+} // namespace cartographer
 
-#endif  // CARTOGRAPHER_MAPPING_INTERNAL_MOTION_FILTER_H_
+#endif // CARTOGRAPHER_MAPPING_INTERNAL_MOTION_FILTER_H_
