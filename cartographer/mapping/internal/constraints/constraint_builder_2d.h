5171f3f45d8d78dff0ae6ac2d6a9a85960d04250
Vu-Hoi HUYNH <vuhoi.huynh@gmail.com>
2023-08-18 13:36:22 +0000

    format: constraint builder 2d

--- cartographer/mapping/internal/constraints/constraint_builder_2d.h
+++ cartographer/mapping/internal/constraints/constraint_builder_2d.h
@@ -27,6 +27,7 @@
 #include "Eigen/Core"
 #include "Eigen/Geometry"
 #include "absl/synchronization/mutex.h"
+
 #include "cartographer/common/fixed_ratio_sampler.h"
 #include "cartographer/common/histogram.h"
 #include "cartographer/common/math.h"
@@ -58,15 +59,17 @@ transform::Rigid2d ComputeSubmapPose(const Submap2D& submap);
 //
 // This class is thread-safe.
 class ConstraintBuilder2D {
- public:
+public:
   using Constraint = PoseGraphInterface::Constraint;
-  using Result = std::vector<Constraint>;
+  using Result     = std::vector<Constraint>;
 
-  ConstraintBuilder2D(const proto::ConstraintBuilderOptions& options,
-                      common::ThreadPoolInterface* thread_pool);
+  ConstraintBuilder2D(
+    const proto::ConstraintBuilderOptions& options,
+    common::ThreadPoolInterface* thread_pool
+  );
   ~ConstraintBuilder2D();
 
-  ConstraintBuilder2D(const ConstraintBuilder2D&) = delete;
+  ConstraintBuilder2D(const ConstraintBuilder2D&)            = delete;
   ConstraintBuilder2D& operator=(const ConstraintBuilder2D&) = delete;
 
   // Schedules exploring a new constraint between 'submap' identified by
@@ -75,10 +78,13 @@ class ConstraintBuilder2D {
   //
   // The pointees of 'submap' and 'compressed_point_cloud' must stay valid until
   // all computations are finished.
-  void MaybeAddConstraint(const SubmapId& submap_id, const Submap2D* submap,
-                          const NodeId& node_id,
-                          const TrajectoryNode::Data* const constant_data,
-                          const transform::Rigid2d& initial_relative_pose);
+  void MaybeAddConstraint(
+    const SubmapId& submap_id,
+    const Submap2D* submap,
+    const NodeId& node_id,
+    const TrajectoryNode::Data* const constant_data,
+    const transform::Rigid2d& initial_relative_pose
+  );
 
   // Schedules exploring a new constraint between 'submap' identified by
   // 'submap_id' and the 'compressed_point_cloud' for 'node_id'.
@@ -87,8 +93,11 @@ class ConstraintBuilder2D {
   // The pointees of 'submap' and 'compressed_point_cloud' must stay valid until
   // all computations are finished.
   void MaybeAddGlobalConstraint(
-      const SubmapId& submap_id, const Submap2D* submap, const NodeId& node_id,
-      const TrajectoryNode::Data* const constant_data);
+    const SubmapId& submap_id,
+    const Submap2D* submap,
+    const NodeId& node_id,
+    const TrajectoryNode::Data* const constant_data
+  );
 
   // Must be called after all computations related to one node have been added.
   void NotifyEndOfNode();
@@ -106,30 +115,33 @@ class ConstraintBuilder2D {
 
   static void RegisterMetrics(metrics::FamilyFactory* family_factory);
 
- private:
+private:
   struct SubmapScanMatcher {
     const Grid2D* grid = nullptr;
     std::unique_ptr<scan_matching::FastCorrelativeScanMatcher2D>
-        fast_correlative_scan_matcher;
+      fast_correlative_scan_matcher;
     std::weak_ptr<common::Task> creation_task_handle;
   };
 
   // The returned 'grid' and 'fast_correlative_scan_matcher' must only be
   // accessed after 'creation_task_handle' has completed.
   const SubmapScanMatcher* DispatchScanMatcherConstruction(
-      const SubmapId& submap_id, const Grid2D* grid)
-      EXCLUSIVE_LOCKS_REQUIRED(mutex_);
+    const SubmapId& submap_id, const Grid2D* grid
+  ) EXCLUSIVE_LOCKS_REQUIRED(mutex_);
 
   // Runs in a background thread and does computations for an additional
   // constraint, assuming 'submap' and 'compressed_point_cloud' do not change
   // anymore. As output, it may create a new Constraint in 'constraint'.
-  void ComputeConstraint(const SubmapId& submap_id, const Submap2D* submap,
-                         const NodeId& node_id, bool match_full_submap,
-                         const TrajectoryNode::Data* const constant_data,
-                         const transform::Rigid2d& initial_relative_pose,
-                         const SubmapScanMatcher& submap_scan_matcher,
-                         std::unique_ptr<Constraint>* constraint)
-      LOCKS_EXCLUDED(mutex_);
+  void ComputeConstraint(
+    const SubmapId& submap_id,
+    const Submap2D* submap,
+    const NodeId& node_id,
+    bool match_full_submap,
+    const TrajectoryNode::Data* const constant_data,
+    const transform::Rigid2d& initial_relative_pose,
+    const SubmapScanMatcher& submap_scan_matcher,
+    std::unique_ptr<Constraint>* constraint
+  ) LOCKS_EXCLUDED(mutex_);
 
   void RunWhenDoneCallback() LOCKS_EXCLUDED(mutex_);
 
@@ -138,8 +150,7 @@ class ConstraintBuilder2D {
   absl::Mutex mutex_;
 
   // 'callback' set by WhenDone().
-  std::unique_ptr<std::function<void(const Result&)>> when_done_
-      GUARDED_BY(mutex_);
+  std::unique_ptr<std::function<void(const Result&)>> when_done_ GUARDED_BY(mutex_);
 
   // TODO(gaschler): Use atomics instead of mutex to access these counters.
   // Number of the node in reaction to which computations are currently
@@ -159,8 +170,7 @@ class ConstraintBuilder2D {
   std::deque<std::unique_ptr<Constraint>> constraints_ GUARDED_BY(mutex_);
 
   // Map of dispatched or constructed scan matchers by 'submap_id'.
-  std::map<SubmapId, SubmapScanMatcher> submap_scan_matchers_
-      GUARDED_BY(mutex_);
+  std::map<SubmapId, SubmapScanMatcher> submap_scan_matchers_ GUARDED_BY(mutex_);
   std::map<SubmapId, common::FixedRatioSampler> per_submap_sampler_;
 
   scan_matching::CeresScanMatcher2D ceres_scan_matcher_;
@@ -169,8 +179,8 @@ class ConstraintBuilder2D {
   common::Histogram score_histogram_ GUARDED_BY(mutex_);
 };
 
-}  // namespace constraints
-}  // namespace mapping
-}  // namespace cartographer
+} // namespace constraints
+} // namespace mapping
+} // namespace cartographer
 
-#endif  // CARTOGRAPHER_MAPPING_INTERNAL_CONSTRAINTS_CONSTRAINT_BUILDER_2D_H_
+#endif // CARTOGRAPHER_MAPPING_INTERNAL_CONSTRAINTS_CONSTRAINT_BUILDER_2D_H_
